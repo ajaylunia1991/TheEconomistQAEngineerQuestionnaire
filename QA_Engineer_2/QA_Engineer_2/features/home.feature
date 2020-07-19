53f542e450d7928728a0b4b130053a98ea71f7e2
Ajaylunia1991 <Ajaylunia1991@gmail.com>
2020-07-20 00:06:16 +0530

    Each of the links in the navigation bar is functional and goes to the correct page.  - Clicking on a sector shows a list of jobs from only that sector. Clicking further on a job listing shows you the job details, with an 'apply' button.  - Searching for a job correctly displays relevant search results.  - Ensuring all the links in the footer are functional All the above mentioned scenarios has been covered in this commit

--- QA_Engineer_2/QA_Engineer_2/features/home.feature
+++ QA_Engineer_2/QA_Engineer_2/features/home.feature
@@ -1,7 +1,7 @@
 Feature: Jobs page
 
   Scenario: Navigation bar renders
-    Given I go to the jobs page
+    Given I go to the jobs.economist.com page
     Then I should see the navigation bar
     * I should see the search fields
     * I should see sector lists
@@ -10,17 +10,17 @@ Feature: Jobs page
     * I should see footer
 
   Scenario: Verify Navigation of Sign In
-    Given I go to the jobs page
+    Given I go to the jobs.economist.com page
     When I click Sign In
     Then I should be landed on Sign In Page
 
   Scenario: Verify Navigation of Create Account
-    Given I go to the jobs page
+    Given I go to the jobs.economist.com page
     When I click Create Account
     Then I should be landed on Create Account Page
 
   Scenario: Verify links in the navigation bar  
-    Given I go to the jobs page 
+    Given I go to the jobs.economist.com page 
     When I click on Home Link in Navigation bar
     Then I should be landed on Home page
     When I click on Find a job Link in Navigation bar
@@ -32,3 +32,38 @@ Feature: Jobs page
     When I click on jobs blog link in Navigation bar
     Then I should be landed on careers page
 
+  Scenario: Verify Jobs Links flow
+      Given I go to the jobs.economist.com page
+      When I click the first job in browse by sector category
+      Then I should be landed on the job category page
+      When I click on the first job of the selected sector
+      Then I should see the option to Apply for the job
+
+
+  Scenario: Verify Job search results
+      Given I go to the jobs.economist.com page
+      When I search for job with name as Head of Operational Risk
+      Then I should see Head of Operational Risk as search result
+
+
+  Scenario: Ensuring all the links in the footer are functional  
+    Given I go to the jobs.economist.com page 
+    When I click on About Us link in Footer area 
+    Then I should be landed on About Us page 
+    When I click on Contact Us link in Footer area
+    Then I should be landed on Contact Us page
+    When I click on Terms & Conditions link in Footer area 
+    Then I should be landed on Terms & Conditions page
+    When I click on Privacy Policy link in Footer area 
+    Then I should be landed on Privacy Policy page 
+    When I click on Advertise with us link in Footer area
+    Then I should be landed on Advertise with us page
+    Given I go to the jobs.economist.com page
+    When I click on Facebook Icon in Footer area
+    Then I should be landed on Facebook economist careers page
+    Given I go to the jobs.economist.com page
+    When I click on Twitter Icon in Footer area 
+    Then I should be landed on Twitter economist careers page
+    Given I go to the jobs.economist.com page
+    When I click on LinkedIn Icon in Footer area
+    Then I should be landed on LinkedIn economist careers page
--- QA_Engineer_2/QA_Engineer_2/features/step_definitions/index.js
+++ QA_Engineer_2/QA_Engineer_2/features/step_definitions/index.js
@@ -2,8 +2,9 @@ const { Given, Then, When, And } = require('cucumber');
 const { By } = require('selenium-webdriver');
 const World = require('../support/world');
 const assert = require('assert').strict;
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-Given(/^I go to the jobs page$/, () => World.goToJobsPage());
+Given(/^I go to the jobs.economist.com page$/, () => World.goToJobsPage());
 
 Then(/^I should see the navigation bar$/, async () => {
   return World.driver.findElement(By.id('primary-nav'));
@@ -13,7 +14,6 @@ Then(/^I should see the navigation bar$/, async () => {
 Then(/^I should see the search fields$/, async () => {
   return assert.ok(World.driver.findElement(By.xpath('//input[@id="keywords"]')).isDisplayed() && World.driver.findElement(By.xpath('//input[@id="location"]')).isDisplayed());
 })
-
 
 
 Then(/^I should see sector lists$/, async () => {
@@ -114,11 +114,10 @@ return assert.ok(currentUrl.includes('register'));
 
 })
 
-console.log('-------------------------------- Links on Navigation Bar----------------------------------------');
 
 //verifying home link in Navigation Bar
 When(/^I click on Home Link in Navigation bar$/, async () => {
-
+  console.log('-------------------------------- Links on Navigation Bar----------------------------------------');
   return await World.driver.findElement(By.xpath('//a[contains(text(),"Home")]')).click();
   
   })
@@ -135,7 +134,7 @@ Then(/^I should be landed on Home page$/, async () => {
 
 //verifying Find a job link in Navigation Bar
 When(/^I click on Find a job Link in Navigation bar$/, async () => {
-
+  await snooze(2000);
   return await World.driver.findElement(By.xpath('//a[contains(text(),"Find a job")]')).click();
   
   })
@@ -153,7 +152,7 @@ Then(/^I should be landed on jobs page$/, async () => {
   
 //verifying job alerts link in Navigation Bar  
 When(/^I click on job alerts link in Navigation bar$/, async () => {
-
+  await snooze(2000);
   return await World.driver.findElement(By.xpath('//a[contains(text(),"Job alerts")]')).click();
   
   })
@@ -170,7 +169,7 @@ Then(/^I should be landed on job alerts page$/, async () => {
 
 //verifying search recruiters link in Navigation Bar  
 When(/^I click on search recruiters Link in Navigation bar$/, async () => {
-
+  await snooze(2000);
   return await World.driver.findElement(By.xpath('//a[contains(text(),"Search recruiters")]')).click();
   
   })
@@ -188,7 +187,7 @@ Then(/^I should be landed on search recruiters page$/, async () => {
 
 //verifying jobs blog link in Navigation Bar  
 When(/^I click on jobs blog link in Navigation bar$/, async () => {
-
+  await snooze(2000);
   return await World.driver.findElement(By.xpath('//a[contains(text(),"Jobs blog")]')).click();
   
   })
@@ -203,5 +202,248 @@ Then(/^I should be landed on careers page$/, async () => {
      
   })
 
+/****************
+ Scenario: Verify Jobs Links flow
+ *****************/
+
+
+
+
+When(/^I click the first job in browse by sector category$/, async () => {
+
+  let jobs = await World.driver.findElements(By.xpath('//div[@class="browse__items"]//a'));
+  
+  for(let job of jobs)
+  {
+        global.text = await job.getText();
+        console.log(text);
+        await job.click();
+        break;
+   }
+  
+  return true;
+  })
+  
+  /****************
+   Link clicked in above when step is matched with the heading of the page opened.
+   *****************/
+  
+  Then(/^I should be landed on the job category page$/, async () => {
+  
+  await snooze(2000);
+  
+  let actualText = await World.driver.findElement(By.xpath('//h1[@id="browsing"]')).getText();
+  
+  return assert.ok(actualText.includes(text));
+  
+  })
+  
+  
+  When(/^I click on the first job of the selected sector$/, async () => {
+  
+  return await World.driver.findElement(By.xpath('//a[@class="js-clickable-area-link"]')).click();
+  
+  })
+  
+  
+  Then(/^I should see the option to Apply for the job$/, async () => {
+  
+  let jobApplications = await World.driver.findElements(By.xpath('//a[contains(text(),"Apply")]'));
+  
+  for(let jobApplication of jobApplications)
+  {
+        return assert.ok(await jobApplication.isDisplayed());
+   }
+  
+  })
+  
+  
+  /****************
+   Scenario: Verify Job search results
+   *****************/
+  
+  When(/^I search for job with name as (.*)$/, async (value) => {
+  
+  await World.driver.findElement(By.xpath('//input[@id="keywords"]')).sendKeys(value);
+  
+  return World.driver.findElement(By.xpath('//input[@value="Search"]')).click();
+  })
+  
+  
+  Then(/^I should see (.*) as search result$/, async (value) => {
+  
+  await snooze(2000);
+  
+  let text = await World.driver.findElement(By.xpath('//a[@class="js-clickable-area-link"]/span')).getText();
+  console.log(value);
+  console.log(text);
+  
+  return assert.ok(text == value);
+  })
   
 
+//verifying About us link is functional in Footer  
+When(/^I click on About Us link in Footer area$/, async () => {
+console.log('--------------Ensuring all the links in the footer are functional------------------');
+let element = await World.driver.findElement(By.xpath('//a[contains(text(),"About Us")]'));
+await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+await snooze(2000);
+return element.click();
+})
+  
+  
+Then(/^I should be landed on About Us page$/, async () => {
+  
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  return assert.ok(currentUrl.includes('about-us'));
+     
+  })
+
+//verifying Contact us link is functional in Footer  
+When(/^I click on Contact Us link in Footer area$/, async () => {
+  let element = World.driver.findElement(By.xpath('//a[contains(text(),"Contact Us")]'));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();
+  })
+  
+  
+Then(/^I should be landed on Contact Us page$/, async () => {
+  
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  return assert.ok(currentUrl.includes('contact-us'));
+     
+  })
+  
+//verifying Terms & Conditions link is functional in Footer  
+When(/^I click on Terms & Conditions link in Footer area$/, async () => {
+  let element = World.driver.findElement(By.xpath('//a[contains(text(),"Terms & Conditions")]'));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();
+  })
+  
+  
+Then(/^I should be landed on Terms & Conditions page$/, async () => {
+  
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  return assert.ok(currentUrl.includes('terms-and-conditions'));
+     
+  })
+  
+//verifying privacy policy link is functional in Footer  
+When(/^I click on Privacy Policy link in Footer area$/, async () => {
+  let element = World.driver.findElement(By.xpath('//a[contains(text(),"Privacy Policy")]'));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();
+  
+  })
+  
+  
+Then(/^I should be landed on Privacy Policy page$/, async () => {
+  
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  return assert.ok(currentUrl.includes('privacy-policy'));
+     
+  })
+
+//verifying advertise with us link is functional in Footer  
+When(/^I click on Advertise with us link in Footer area$/, async () => {
+  let element = World.driver.findElement(By.xpath('//a[contains(text(),"Advertise with us")]'));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();  
+  })
+  
+  
+Then(/^I should be landed on Advertise with us page$/, async () => {
+  
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  return assert.ok(currentUrl.includes('recruiters.jobs.economist.com'));
+     
+  })
+
+//verifying Facebook Icon link is functional in Footer  
+When(/^I click on Facebook Icon in Footer area$/, async () => {
+  let element = World.driver.findElement(By.css(".social-buttons__item.social-buttons__item--fb"));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();
+  })
+  
+  
+Then(/^I should be landed on Facebook economist careers page$/, async () => {
+  let tabs = await (await World.driver).getAllWindowHandles();
+  console.log(tabs);
+  await (await World.driver).switchTo().window((tabs[1]));
+ 
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  assert.ok(currentUrl.includes('facebook.com/economistcareersnetwork'));
+  
+  await World.driver.close();
+
+  return (await (await World.driver).switchTo().window((tabs[0])));
+  
+  })
+
+//verifying Twitter Icon link is functional in Footer  
+When(/^I click on Twitter Icon in Footer area$/, async () => {
+  let element = World.driver.findElement(By.css(".social-buttons__item.social-buttons__item--tw"));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();  
+  })
+  
+  
+Then(/^I should be landed on Twitter economist careers page$/, async () => {
+  let tabs = await (await World.driver).getAllWindowHandles();
+  console.log(tabs);
+  await (await World.driver).switchTo().window((tabs[1]));
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  assert.ok(currentUrl.includes('twitter.com/careersnetwork'));
+
+  await World.driver.close();
+
+  return (await (await World.driver).switchTo().window((tabs[0])));
+     
+  })
+
+//verifying LinkedIn Icon link is functional in Footer  
+When(/^I click on LinkedIn Icon in Footer area$/, async () => {
+  let element = World.driver.findElement(By.css(".social-buttons__item.social-buttons__item--in"));
+  await World.driver.executeScript("arguments[0].scrollIntoView(true);",element);
+  await snooze(2000);
+  return element.click();
+  })
+  
+  
+Then(/^I should be landed on LinkedIn economist careers page$/, async () => {
+  let tabs = await (await World.driver).getAllWindowHandles();
+  console.log(tabs);
+  await (await World.driver).switchTo().window((tabs[1]));
+ 
+  let currentUrl = await World.driver.getCurrentUrl();
+  console.log(currentUrl);
+  
+  assert.ok(currentUrl.includes('linkedin.com/company/the-economist-careers-network'));
+  
+  await World.driver.close();
+
+  return (await (await World.driver).switchTo().window((tabs[0])));
+  
+  })
